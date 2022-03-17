28c8f4c5d8ea55833cea2e0afd29379f44fbf31a
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-17 15:09:39 -0400

    Added css loader to webpack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
 import { hot } from 'react-hot-loader';
-import Test from './components/Test';
 
 const App = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
-            <Test />
             <button onClick={() => { setCount(count => count + 1) }}>Click Me: {count}</button>
         </div>
     )
