0c65f6c6039aec74414d146c93f69f542f35d05d
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-17 14:36:42 -0400

    Added react hot loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import { hot } from 'react-hot-loader';
+import Test from './components/Test';
 
 const App = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
+            <Test />
             <button onClick={() => { setCount(count => count + 1) }}>Click Me: {count}</button>
         </div>
     )
 }
 
-export default App
+export default hot(module)(App)
